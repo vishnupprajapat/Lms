5cdd4455466f2e7b95390eb860b32234fca73b50
vishnupprajapat <prajapatvishnu767@gmail.com>
2024-05-03 19:36:50 +0530

    create course api

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -28,3 +28,17 @@ export const isAuthenticated = catchAsyncErrors(
     next();
   }
 );
+//validate user role
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role || "")) {
+      return next(
+        new ErrorHandler(
+          `Role ${req.user?.role || ""} is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
