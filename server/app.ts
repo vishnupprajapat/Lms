a4836f1912e8b47c0b53656dc9c3b0231f1e6c96
vishnupprajapat <prajapatvishnu767@gmail.com>
2024-05-20 13:15:53 +0530

    create analytics api

--- server/app.ts
+++ server/app.ts
@@ -7,6 +7,7 @@ import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
 import notificationRouter from "./routes/notification.route";
+import analyticsRouter from "./routes/analytics.route";
 
 export const app = express();
 
@@ -24,7 +25,14 @@ app.use(
   })
 );
 //routes
-app.use("/api/v1", userRouter, courseRouter, orderRouter, notificationRouter);
+app.use(
+  "/api/v1",
+  userRouter,
+  courseRouter,
+  orderRouter,
+  notificationRouter,
+  analyticsRouter
+);
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
