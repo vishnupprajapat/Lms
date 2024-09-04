9f3d95860f72d7c1c745d7f22b5c4b1fa6e765e4
vishnu prajapat <vishnu@cmsminds.com>
2024-09-04 15:24:05 +0530

    increese db-connect time

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -5,9 +5,14 @@ const dbUrl: string = process.env.DB_URI || "";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(dbUrl).then((data: any) => {
-      console.log(`Database connected with ${data.connection.host}`);
-    });
+    await mongoose
+      .connect(dbUrl, {
+        serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+        socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+      })
+      .then((data: any) => {
+        console.log(`Database connected with ${data.connection.host}`);
+      });
   } catch (error: any) {
     console.log(error.message);
     setTimeout(connectDb, 5000);
